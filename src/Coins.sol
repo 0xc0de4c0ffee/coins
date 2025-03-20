b2fe47013098e4aa7c1087922d43db2af65b68d2
ross <92001561+z0r0z@users.noreply.github.com>
2025-03-20 19:44:33 +0000

    smolr

--- src/Coins.sol
+++ src/Coins.sol
@@ -1,21 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.29;
 
-import {ERC6909} from "@solady/src/tokens/ERC6909.sol";
+contract Coins {
+    event MetadataSet(uint256 indexed);
+    event OwnershipTransferred(uint256 indexed);
 
-contract Coins is ERC6909 {
-    event MetadataSet(uint256 indexed id);
-    event OwnershipTransferred(uint256 indexed id);
+    event OperatorSet(address indexed, address indexed, bool);
+    event Approval(address indexed, address indexed, uint256 indexed, uint256);
+    event Transfer(address, address indexed, address indexed, uint256 indexed, uint256);
 
     error Unauthorized();
     error AlreadyCreated();
     error InvalidMetadata();
 
     mapping(uint256 id => Metadata) public _metadata;
-
     mapping(uint256 id => address owner) public ownerOf;
-
     mapping(uint256 id => uint256) public totalSupply;
+
+    mapping(address => mapping(address => bool)) public isOperator;
+    mapping(address => mapping(uint256 => uint256)) public balanceOf;
+    mapping(address => mapping(address => mapping(uint256 => uint256))) public allowance;
 
     modifier onlyOwnerOf(uint256 id) {
         require(msg.sender == ownerOf[id], Unauthorized());
@@ -30,19 +34,19 @@ contract Coins is ERC6909 {
         string tokenURI;
     }
 
-    function name(uint256 id) public view override(ERC6909) returns (string memory) {
+    function name(uint256 id) public view returns (string memory) {
         return _metadata[id].name;
     }
 
-    function symbol(uint256 id) public view override(ERC6909) returns (string memory) {
+    function symbol(uint256 id) public view returns (string memory) {
         return _metadata[id].symbol;
     }
 
-    function decimals(uint256) public pure override(ERC6909) returns (uint8) {
+    function decimals(uint256) public pure returns (uint8) {
         return 18;
     }
 
-    function tokenURI(uint256 id) public view override(ERC6909) returns (string memory) {
+    function tokenURI(uint256 id) public view returns (string memory) {
         return _metadata[id].tokenURI;
     }
 
@@ -63,21 +67,17 @@ contract Coins is ERC6909 {
 
         _metadata[id] = Metadata(_name, _symbol, _tokenURI);
 
-        _mint(ownerOf[id] = owner, id, totalSupply[id] = supply);
+        _mint(ownerOf[id] = owner, id, supply);
     }
 
     // COIN ID MINT/BURN
 
     function mint(address to, uint256 id, uint256 amount) public onlyOwnerOf(id) {
-        totalSupply[id] += amount;
         _mint(to, id, amount);
     }
 
     function burn(uint256 id, uint256 amount) public {
         _burn(msg.sender, id, amount);
-        unchecked {
-            totalSupply[id] -= amount;
-        }
     }
 
     // COIN ID GOVERNANCE
@@ -100,17 +100,96 @@ contract Coins is ERC6909 {
     // COIN ID WRAPPING
 
     function wrap(address asset, uint256 amount) public {
-        totalSupply[uint256(uint160(asset))] += amount;
         _mint(msg.sender, uint256(uint160(asset)), amount);
         IERC20(asset).transferFrom(msg.sender, address(this), amount);
     }
 
     function unwrap(address asset, uint256 amount) public {
         _burn(msg.sender, uint256(uint160(asset)), amount);
-        unchecked {
-            totalSupply[uint256(uint160(asset))] -= amount;
-        }
         IERC20(asset).transfer(msg.sender, amount);
+    }
+
+    // ERC6909
+
+    function transfer(address to, uint256 id, uint256 amount) public returns (bool) {
+        balanceOf[msg.sender][id] -= amount;
+
+        // Cannot overflow because the sum of all user
+        // balances can't exceed the max uint256 value.
+        unchecked {
+            balanceOf[to][id] += amount;
+        }
+
+        emit Transfer(msg.sender, msg.sender, to, id, amount);
+
+        return true;
+    }
+
+    function transferFrom(address from, address to, uint256 id, uint256 amount)
+        public
+        returns (bool)
+    {
+        if (msg.sender != from && !isOperator[from][msg.sender]) {
+            uint256 allowed = allowance[from][msg.sender][id];
+            if (allowed != type(uint256).max) allowance[from][msg.sender][id] = allowed - amount;
+        }
+
+        balanceOf[from][id] -= amount;
+
+        // Cannot overflow because the sum of all user
+        // balances can't exceed the max uint256 value.
+        unchecked {
+            balanceOf[to][id] += amount;
+        }
+
+        emit Transfer(msg.sender, from, to, id, amount);
+
+        return true;
+    }
+
+    function approve(address to, uint256 id, uint256 amount) public returns (bool) {
+        allowance[msg.sender][to][id] = amount;
+        emit Approval(msg.sender, to, id, amount);
+        return true;
+    }
+
+    function setOperator(address operator, bool approved) public returns (bool) {
+        isOperator[msg.sender][operator] = approved;
+        emit OperatorSet(msg.sender, operator, approved);
+        return true;
+    }
+
+    // ERC165
+
+    function supportsInterface(bytes4 interfaceId) public pure returns (bool) {
+        return interfaceId == 0x01ffc9a7 // ERC165 Interface ID for ERC165.
+            || interfaceId == 0x0f632fb3; // ERC165 Interface ID for ERC6909.
+    }
+
+    // INTERNAL MINT/BURN
+
+    function _mint(address to, uint256 id, uint256 amount) internal {
+        totalSupply[id] += amount;
+
+        // Cannot overflow because the sum of all user
+        // balances can't exceed the max uint256 value.
+        unchecked {
+            balanceOf[to][id] += amount;
+        }
+
+        emit Transfer(msg.sender, address(0), to, id, amount);
+    }
+
+    function _burn(address from, uint256 id, uint256 amount) internal {
+        balanceOf[from][id] -= amount;
+
+        // Cannot underflow because a user's balance
+        // will never be larger than the total supply.
+        unchecked {
+            totalSupply[id] -= amount;
+        }
+
+        emit Transfer(msg.sender, from, address(0), id, amount);
     }
 }
 
